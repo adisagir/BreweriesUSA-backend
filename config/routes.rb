0b5ba3c8dacd0e4d0ca10fa0100b07d1314b5eeb
github username <github email address>
2019-07-10 11:55:08 -0400

    added some routes and updated review controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,10 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   post "/login", to: "smsbackend#login"
   post "/signup", to: "users#create"
+  post "/review", to: "reviews#create"
   get "/profile", to: "users#profile"
 
   resources :users, only: [:new, :index, :show]
   resources :breweries, only: [:index, :show]
+  resources :reviews, only: [:show, :create, :index]
 end
